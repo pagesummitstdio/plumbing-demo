a89e2929cc82eda998f3da5b6acc7a9d3a5cef89
ashtonloredo <ashtonloredo@gmail.com>
2026-07-11 23:15:43 -0500

    Ready for GitHub push

--- Resources/js/script.js
+++ Resources/js/script.js
@@ -1,6 +1,8 @@
 const navButton = document.getElementById("phone-nav-btn");
 const navMenu = document.getElementById("nav-menu");
 
-navButton.addEventListener("click", () => {
-    navMenu.classList.toggle("active");
-});
+if (navButton && navMenu) {
+    navButton.addEventListener("click", () => {
+        navMenu.classList.toggle("active");
+    });
+}
