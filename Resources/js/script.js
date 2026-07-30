b775aa733c1503ad800c09685de790ec2b7f6763
Page Summit <page.summit.stdio@gmail.com>
2026-07-30 16:49:00 -0500

    Improved Nav Menu button

--- Resources/js/script.js
+++ Resources/js/script.js
@@ -1,8 +1,26 @@
+const page = document.getElementById("page-body");
 const navButton = document.getElementById("phone-nav-btn");
 const navMenu = document.getElementById("nav-menu");
 
-if (navButton && navMenu) {
-    navButton.addEventListener("click", () => {
-        navMenu.classList.toggle("active");
-    });
-}
+navButton.addEventListener("click", () => {
+    navMenu.classList.toggle("active");
+    if (navButton.textContent === "☰") {
+        navButton.textContent = "×";
+        navButton.classList.add("close-icon");
+    } else {
+        navButton.textContent = "☰";
+        navButton.classList.remove("close-icon");
+    }
+});
+
+document.addEventListener("click", (e) => {
+    const clickedInsideMenu = navMenu.contains(e.target);
+    const clickedNavButton = navButton.contains(e.target);
+    const clickedNavLink = e.target.tagName === "A" && clickedInsideMenu;
+
+    if (navMenu.classList.contains("active") && (!clickedInsideMenu || clickedNavLink) && !clickedNavButton) {
+        navMenu.classList.remove("active");
+        navButton.innerHTML = "☰"; 
+        navButton.classList.remove("close-icon");  
+    }
+});
